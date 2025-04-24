95cf8372afd0977c6c81cf16d04b22c8a536d7ed
Prerana Sangar <146517815+PreranaSangar@users.noreply.github.com>
2025-04-24 10:05:59 +0530

    Add files via upload

---
+++ importing csvinto sql.sql
@@ -0,0 +1,16 @@
+Drop table if exists employee3;
+
+CREATE TABLE employee3(
+    employee_id text,
+    first_name text,
+    last_name text,
+    email text,
+    department text,
+    salary text,
+    joining_date text,
+    age text
+);
+
+select * from employee3;
+
+
